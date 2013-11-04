2b92687a647eccf2fe3c017f3920e565811c131c
NitinS <nitinsg1981@gmail.com>
2013-11-05 04:50:49 +0800

    Initial commit for market data subscriber.

---
+++ MarketDataSubscriber/src/main/java/ns/freetime/MarketSubscriberMain.java
@@ -0,0 +1,20 @@
+package ns.freetime;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MarketSubscriberMain 
+{
+    private static Logger log = LogManager.getLogger( MarketSubscriberMain.class);
+    
+    public static void main( String[] args )
+    {
+	//Startup all gateways
+	
+	//Ignite the Disruptor ring
+	
+	//Launch the UI
+	
+        log.info( "..." );
+    }
+}
